f2a21234a163784aa8912f1a656bcaab170105f0
cs50nurd <141682007+cs50nurd@users.noreply.github.com>
2025-06-03 13:46:43 -0500

    Add files via upload

---
+++ gambachess.js
@@ -0,0 +1,12 @@
+// gambachess.js
+
+// Strict mode helps catch common coding errors and "unsafe" actions
+'use strict';
+
+// Entry point function
+function main() {
+    console.log('Welcome to Gamba Chess!');
+}
+
+// Execute the main function
+main();
